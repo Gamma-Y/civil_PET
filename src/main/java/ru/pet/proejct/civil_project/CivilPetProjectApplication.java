9351eb9198c0e94ae493f6b862a2cf7daad86dfd
Evgeny Smirnov <HoUsToN.97@mail.ru>
2025-01-14 22:35:05 +0500

    project init

---
+++ src/main/java/ru/pet/proejct/civil_project/CivilPetProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.pet.proejct.civil_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivilPetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CivilPetProjectApplication.class, args);
+    }
+
+}
