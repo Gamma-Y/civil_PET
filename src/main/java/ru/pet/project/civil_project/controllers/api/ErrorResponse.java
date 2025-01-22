68244b57a81276dc6bb8d75a46b1120b8af45645
Evgeny Smirnov <HoUsToN.97@mail.ru>
2025-01-23 00:15:21 +0500

    update controller for Exception

---
+++ src/main/java/ru/pet/project/civil_project/controllers/api/ErrorResponse.java
@@ -0,0 +1,9 @@
+package ru.pet.project.civil_project.controllers.api;
+
+/**
+ * @author Gamma on 23.01.2025
+ */
+
+
+public record ErrorResponse(String errorCode, String message) {
+}
--- src/main/java/ru/pet/project/civil_project/controllers/api/ExceptionController.java
+++ src/main/java/ru/pet/project/civil_project/controllers/api/ExceptionController.java
@@ -1,11 +1,35 @@
 package ru.pet.project.civil_project.controllers.api;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.pet.project.civil_project.exception.BadRequestException;
+import ru.pet.project.civil_project.exception.ResourceNotFoundException;
 
 /**
  * @author Gamma on 20.01.2025
  */
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionController {
-    
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse("RESOURCE_NOT_FOUND", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(BadRequestException ex) {
+        ErrorResponse errorResponse = new ErrorResponse("BAD_REQUEST", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse("INTERNAL_SERVER_ERROR", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
