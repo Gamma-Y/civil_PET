cc4d65148625082656b8a572b72eb29e8d911955
Evgeny Smirnov <HoUsToN.97@mail.ru>
2025-01-22 22:57:28 +0500

    add general service

---
+++ src/main/java/ru/pet/project/civil_project/services/impl/general/CarGeneralService.java
@@ -0,0 +1,57 @@
+package ru.pet.project.civil_project.services.impl.general;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.pet.project.civil_project.db.entities.Car;
+import ru.pet.project.civil_project.db.repositories.CarRepository;
+import ru.pet.project.civil_project.exception.ResourceNotFoundException;
+import ru.pet.project.civil_project.services.GeneralService;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Gamma on 22.01.2025
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CarGeneralService implements GeneralService<Car, Long> {
+    private final CarRepository carRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Car> findAll() {
+        log.debug("Try find all cars");
+        return carRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Car> findById(Long id) {
+        log.debug("Try find car with id: {}", id);
+        return carRepository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Car save(Car entity) {
+        log.debug("Try save car: {}", entity);
+        return carRepository.save(entity);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        log.debug("Try delete car with id: {}", id);
+        carRepository.findById(id)
+                .ifPresentOrElse(
+                        carRepository::delete,
+                        () -> {
+                            throw new ResourceNotFoundException("Car", id);
+                        }
+                );
+    }
+}
---
+++ src/main/java/ru/pet/project/civil_project/services/impl/general/PassportGeneralService.java
@@ -0,0 +1,59 @@
+package ru.pet.project.civil_project.services.impl.general;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.pet.project.civil_project.db.entities.Passport;
+import ru.pet.project.civil_project.db.repositories.PassportRepository;
+import ru.pet.project.civil_project.exception.ResourceNotFoundException;
+import ru.pet.project.civil_project.services.GeneralService;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Gamma on 22.01.2025
+ */
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PassportGeneralService implements GeneralService<Passport, Long> {
+
+    private final PassportRepository passportRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Passport> findAll() {
+        log.debug("Try find all passports");
+        return passportRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Passport> findById(Long id) {
+        log.debug("Try find passport with id: {}", id);
+        return passportRepository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Passport save(Passport entity) {
+        log.debug("Try save passport: {}", entity);
+        return passportRepository.save(entity);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        log.debug("Try delete passport with id: {}", id);
+        passportRepository.findById(id)
+                .ifPresentOrElse(
+                        passportRepository::delete,
+                        () -> {
+                            throw new ResourceNotFoundException("Passport", id);
+                        }
+                );
+    }
+}
